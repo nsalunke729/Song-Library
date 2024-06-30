4832dbb9510affec40dcc0d2acbbf9e282532b62
Niranjan <nsalunke729@gmail.com>
2024-06-30 20:18:23 +0100

    Component added and data content flattened.

--- songautocomplete/src/App.js
+++ songautocomplete/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SongAutocomplete from './songcomplete';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Song Autocomplete</h1>
+            <SongAutocomplete />
+        </div>
+    );
 }
 
 export default App;
---
+++ songautocomplete/src/autocomplete.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import axios from 'axios';
+
+function SongAutocomplete() {
+    const [songs, setSongs] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+
+    useEffect(() => {
+        axios.get('/songs.json').then((response) => {
+            setSongs(response.data);
+        });
+    }, []);
+
+    return (
+        <Autocomplete
+            options={songs}
+            getOptionLabel={(option) => `${option.title} - ${option.artist}`}
+            style={{ width: 300 }}
+            renderInput={(params) => (
+                <TextField {...params} label="Search Songs" variant="outlined" />
+            )}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+                setInputValue(newInputValue);
+            }}
+        />
+    );
+}
+
+export default SongAutocomplete;
